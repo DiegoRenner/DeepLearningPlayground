47af7d729c5290f12dae5223521e4c1125aefc90
drenner <renner@cscs.ch>
2021-05-06 01:24:38 +0200

    emergency push for availability

---
+++ learning_reaction-convection-diffusion_equation_ver0.py
@@ -0,0 +1,114 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils
+import torch.utils.data
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+opt_type = "LBFGS"
+
+fname = os.path.join("Task1/TrainingData.txt")
+values = np.loadtxt(fname, skiprows=1, delimiter=",")
+
+
+#print(max(t), max(T0[:,0]), max(T0[:,1]))
+t = torch.from_numpy(values[:,0].transpose()).reshape((265, 1)).float()/1e6
+#Tf0 = values[:,1]
+#Ts0 = values[:,2]
+T0 = torch.from_numpy(values[:,1:3]).float()
+T0[:,0] /= 1e3
+T0[:,1] /= 1e3
+n_samples = t.shape[0]
+batch_size = n_samples
+training_set = DataLoader(torch.utils.data.TensorDataset(t, T0), batch_size=batch_size, shuffle=True)
+
+class NeuralNet(nn.Module):
+
+    def __init__(self, input_dimension, output_dimension, n_hidden_layers, neurons):
+        super(NeuralNet, self).__init__()
+        # Number of input dimensions n
+        self.input_dimension = input_dimension
+        # Number of output dimensions m
+        self.output_dimension = output_dimension
+        # Number of neurons per layer
+        self.neurons = neurons
+        # Number of hidden layers
+        self.n_hidden_layers = n_hidden_layers
+        # Activation function
+        self.activation = nn.Tanh()
+
+        self.input_layer = nn.Linear(self.input_dimension, self.neurons)
+        self.hidden_layers = nn.ModuleList([nn.Linear(self.neurons, self.neurons) for _ in range(n_hidden_layers)])
+        self.output_layer = nn.Linear(self.neurons, self.output_dimension)
+
+    def forward(self, x):
+        # The forward function performs the set of affine and non-linear transformations defining the network
+        # (see equation above)
+        x = self.activation(self.input_layer(x))
+        for k, l in enumerate(self.hidden_layers):
+            x = self.activation(l(x))
+        return self.output_layer(x)
+
+
+
+my_network = NeuralNet(input_dimension=1, output_dimension=2, n_hidden_layers=4, neurons=20)
+#my_network.double()
+
+def init_xavier(model):
+    def init_weights(m):
+        if type(m) == nn.Linear and m.weight.requires_grad and m.bias.requires_grad:
+            g = nn.init.calculate_gain('tanh')
+            torch.nn.init.xavier_uniform_(m.weight, gain=g)
+            #torch.nn.init.xavier_normal_(m.weight, gain=g)
+            m.bias.data.fill_(0)
+    model.apply(init_weights)
+
+# Xavier weight initialization
+init_xavier(my_network)
+
+if opt_type == "ADAM":
+    optimizer_ = optim.Adam(my_network.parameters(), lr=0.001)
+elif opt_type == "LBFGS":
+    optimizer_ = optim.LBFGS(my_network.parameters(), lr=0.1, max_iter=1, max_eval=50000, tolerance_change=1.0 * np.finfo(float).eps)
+else:
+    raise ValueError("Optimizer not recognized")
+
+def fit(model, training_set, num_epochs, optimizer, p, verbose=True):
+    history = list()
+
+    # Loop over epochs
+    for epoch in range(num_epochs):
+        if verbose: print("################################ ", epoch, " ################################")
+
+        running_loss = list([0])
+
+        # Loop over batches
+        for j, (x_train_, u_train_) in enumerate(training_set):
+
+            def closure():
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                # forward + backward + optimize
+                u_pred_ = model(x_train_)
+                # Item 1. below
+                loss = torch.mean((u_pred_.reshape(-1, ) - u_train_.reshape(-1, ))**p)
+
+                # Item 2. below
+                loss.backward()
+                # Compute average training loss over batches for the current epoch
+                running_loss[0] += loss.item()
+                return loss
+
+            # Item 3. below
+            optimizer.step(closure=closure)
+
+        print('Loss: ', (running_loss[0] / len(training_set)))
+        history.append(running_loss[0])
+
+    return history
+
+n_epochs = 2500
+history = fit(my_network, training_set, n_epochs, optimizer_, p=2, verbose=True )
